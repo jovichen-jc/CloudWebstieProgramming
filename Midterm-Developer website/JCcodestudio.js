e9f1f173cb1024da16be548594b6bbd1889bce5a
jovichen-jc <jovichen.jc@gmail.com>
2026-01-21 19:53:36 -0500

    restart the uploading of mid term project

--- Midterm-Developer website/JCcodestudio.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("actionBtn").onmouseover = function () {
-  this.innerText = "EMAIL YOUR CODING FAIRY NOW :)";
-};
-document.getElementById("actionBtn").onmouseout = function () {
-  this.innerText = "Contact me at 6403345@edu.vaniercollege.qc.ca";
-};
